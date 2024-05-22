15c3d6bf7ed7b3861124af32218b4e76bf88a313
Yoan <mrts.yoan@gmail.com>
2024-05-22 13:56:55 +0200

    Home controller testing

---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeControllerTest extends WebTestCase
+{
+    public function testIndexPageDisplaysOffers()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        $this->assertSelectorExists('#offers');
+
+        $this->assertCount(3, $crawler->filter('#offerTitle'));
+
+        $this->assertSelectorTextContains('#offers', 'Pack Solo');
+        $this->assertSelectorTextContains('#offers', 'Pack Duo');
+        $this->assertSelectorTextContains('#offers', 'Pack Famille');
+    }
+}
