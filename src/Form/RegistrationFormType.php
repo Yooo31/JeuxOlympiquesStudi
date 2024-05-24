7d8fde82589f12f779930289b81b86ab27023a01
Yoan <mrts.yoan@gmail.com>
2024-05-24 18:34:53 +0200

    Design login form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Email;
@@ -29,7 +30,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('phone', null, [
+            ->add('phone', TextType::class, [
                 'constraints' => [
                     new Regex([
                         'pattern' => '/^\d{10}$/',
